59e5f0a923ab2386f1272c271b67c51b68733c55
Lake <89255345+ylake@users.noreply.github.com>
2022-10-22 09:53:34 +0900

    Create read_preference.py

---
+++ 03.읽기쓰기제어/read_preference.py
@@ -0,0 +1,32 @@
+from pymongo import MongoClient
+from pymongo.read_preferences import ReadPreference
+import certifi
+
+conn = "mongodb+srv://__USER__:__PWD__@cluster0.v6fiw3s.mongodb.net/"
+# conn = "mongodb+srv://mongodb_user:mongo1234@cluster0.v6fiw3s.mongodb.net/?readPreference=secondary"
+client = MongoClient(conn, tlsCAFile=certifi.where())
+db = client.test
+
+db.fruits.insert_many([
+	{
+		"name": "melon",
+		"qty": 1000,
+		"price": 16000
+	},
+	{
+		"name": "starberry",
+		"qty": 100,
+		"price": 10000
+	},
+	{
+		"name": "grape",
+		"qty": 1500,
+		"price": 5000
+	},
+])
+
+query_filter = {"name": "melon"}
+while True:
+	res = db.fruits.with_options(read_preference=ReadPreference.SECONDARY).find_one(query_filter)
+	# res = db.fruits.find_one(query_filter)
+	print(res)
