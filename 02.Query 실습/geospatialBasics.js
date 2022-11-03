1a0ab8bea69bd5322dccf9c38bb68c0a9bf9cfed
Lake <89255345+ylake@users.noreply.github.com>
2022-11-03 20:40:49 +0900

    Create geospatialBasics.js

---
+++ 02.Query 실습/geospatialBasics.js
@@ -0,0 +1,130 @@
+db.grids.insertMany([
+	{
+		_id: 1,
+		loc: [0,0]
+	},
+	{
+		_id: 2,
+		loc: [3,4]
+	},
+	{
+		_id: 3,
+		loc: [15,2]
+	},
+	{
+		_id: 4,
+		loc: [7,8]
+	},
+	{
+		_id: 5,
+		loc: {
+			type: "Point",
+			coordinates: [5,5]
+		}
+	},
+	{
+		_id: 6,
+		loc: {
+			type: "Point",
+			coordinates: [14,8]
+		}
+	},
+	{
+		_id: 7,
+		loc: {
+			type: "LineString",
+			coordinates: [
+				[6, 6],
+				[15,13]
+			]
+		}
+	},
+	{
+		_id: 8,
+		loc: {
+			type: "LineString",
+			coordinates: [
+				[0, 12],
+				[5,12]
+			]
+		}
+	},
+	{
+		_id: 9,
+		loc: {
+			type: "Polygon",
+			coordinates: [
+				[
+					[2, 2],
+					[3,3],
+					[4,2],
+					[2,2]
+				]
+			]
+		}
+	},
+	{
+		_id: 10,
+		loc: {
+			type: "Polygon",
+			coordinates: [
+				[
+					[9, 0],
+					[5,13],
+					[14,6],
+					[9,0]
+				]
+			]
+		}
+	},
+])
+
+db.grids.find({
+	loc: {
+		$geoIntersects: {
+			$geometry: {
+				type: "Polygon",
+				coordinates: [
+					[
+						[0,0],
+						[10,0],
+						[10,10],
+						[0,10],
+						[0,0],
+					]
+				]
+			}
+		}
+	}
+})
+
+db.grids.find({
+	loc: {
+		$geoWithin: {
+			$geometry: {
+				type: "Polygon",
+				coordinates: [
+					[
+						[0,0],
+						[10,0],
+						[10,10],
+						[0,10],
+						[0,0],
+					]
+				]
+			}
+		}
+	}
+})
+
+db.grids.find({
+	loc: {
+		$near: {
+			$geometry: {
+				type: "Point",
+				coordinates: [5, 5]
+			},
+			$maxDistance: 3
+		}
+	}
+})
