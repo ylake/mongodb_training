bdf2d49964002a4e369ca13d46d0da25161760e3
Lake <89255345+ylake@users.noreply.github.com>
2022-11-03 22:55:30 +0900

    Update geospatialBasics.js

--- 02.Query 실습/geospatialBasics.js
+++ 02.Query 실습/geospatialBasics.js
@@ -124,7 +124,101 @@ db.grids.find({
 				type: "Point",
 				coordinates: [5, 5]
 			},
-			$maxDistance: 3
+			$maxDistance: 1000000000
 		}
 	}
 })
+
+db.grids.createIndex({loc: "2d"})
+db.grids.createIndex({loc: "2dsphere"})
+
+db.grids.drop()
+
+
+db.grids.insertMany([
+	{
+		_id: 1,
+		loc: [0,0]
+	},
+	{
+		_id: 2,
+		loc: [3,4]
+	},
+	{
+		_id: 3,
+		loc: [15,2]
+	},
+	{
+		_id: 4,
+		loc: [7,8]
+	},
+])
+
+
+db.grids.find({
+	loc: {
+		$near: [5,5],
+		$maxDistance: 10
+	}
+})
+
+// 음식점이 있는 동네
+var restaurant = db.restaurants.findOne()
+db.neighborhoods.find(
+	{
+		geometry: {
+			$geoIntersects: {
+				$geometry: {
+					type: "Point",
+					coordinates: restaurant.address.coord
+				}
+			}
+		}
+	},
+	{
+		name: 1
+	}
+)
+
+var neighborhood = db.neighborhoods.findOne()
+db.restaurants.find(
+	{
+		"address.coord": {
+			$geoWithin: {
+				$geometry: neighborhood.geometry
+			}
+		}
+	},
+	{
+		name: 1, _id: 0
+	}
+)
+
+db.restaurants.createIndex({"address.coord": "2dsphere"})
+
+db.restaurants.aggregate([
+	{
+		$geoNear: {
+			near: {
+				type: "Point",
+				coordinates: [ -73.8845166, 40.744772 ]
+			},
+			key: "address.coord",
+			maxDistance: 3000,
+			query: {
+				cuisine: "Hamburgers"
+			},
+			distanceField: "dist"
+		}
+	},
+	{
+		$project: {
+			name: 1,
+			cuisine: 1,
+			dist: 1
+		}
+	},
+	{
+		$count: "cnt"
+	}
+])
